c8e5f3cf80f830da1090bca09914dda707f7b7bd
Christopher Dixon <30005861@student.toiohomai.ac.nz>
2018-08-01 11:03:37 +1200

    fml

--- Program.cs
+++ Program.cs
@@ -9,25 +9,45 @@ namespace ConsoleApp2
     class Program{
         static void Main(string[] args){ 
             int menu = 0;
+            int[] squarenums = new int[] { 0, 0 };
+            int[] squarenums = new int[] { 0, 0 };
             while (menu != 5){
                 Console.WriteLine("enter 1 to get circle vaules");
                 Console.WriteLine("enter 2 to get circle area");
                 Console.WriteLine("enter 3 to get square vaules");
                 Console.WriteLine("enter 4 to get square area");
-                Console.WriteLine("enter e to close");
+                Console.WriteLine("enter 5 to close");
                 menu = int.Parse(Console.ReadLine());
-                switch(menu)
-                {
+                switch(menu){
+
                     case 1:
+                        Console.WriteLine(Square.squares());
+                        Console.ReadLine
+                        break;
+
+                    case 2:
                         break;
                 }
             }
         }
     }
+
     class Square{
+        static squares[] getsquare(){
+            squares[0] = 1;
+            return squares;
+        }
+        static int squarearea(){
 
+        }
     }
-    class Circle{
 
+    class Circle{
+        static int getcircle(){
+
+        }
+        static int circlearea(){
+
+        }
     }
 }
